f3d4093e309fc07f857ab4c56e920490197887f3
4bah12 <Lgforce14@gmail.com>
2025-10-29 09:42:01 -0400

    d
    id didk ta thingkes

--- enemy.gd
+++ enemy.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 1200.0
+const SPEED = 400.0
 const slow = 10
 func _ready():
 	velocity = Vector2(-200,-200).normalized() * SPEED
@@ -10,13 +10,13 @@ func _physics_process(delta: float) -> void:
 	var collision_info = move_and_collide(velocity*delta)
 	if collision_info:
 		velocity = velocity.bounce(collision_info.get_normal())
-		#velocity.x += SPEED
 
 
-		
+
+
 
 	print(velocity.x)
 	if velocity.x >SPEED :
-		velocity.x = move_toward(velocity.x, SPEED*2, slow)
+		velocity.x = move_toward(velocity.x, SPEED*1, slow)
 	if velocity.x <SPEED*-1:
-		velocity.x = move_toward(velocity.x, SPEED*-2, slow)
+		velocity.x = move_toward(velocity.x, SPEED*-1, slow)

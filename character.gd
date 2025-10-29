010b406ec1f9e5c062690fa08b6ac8ac8dd5dce8
linkman <linkman8912@proton.me>
2025-10-29 11:12:43 -0400

    add player control

--- character.gd
+++ character.gd
@@ -2,13 +2,24 @@ extends CharacterBody2D
 
 
 const SPEED = 400.0
+const maxDegreesPerSecond = 30
+
 func _ready():
 	pass
 	
 func _physics_process(delta: float) -> void:
-	var directionx := Input.get_axis("ui_left", "ui_right")
-	var directiony := Input.get_axis("ui_down", "ui_up")
-	if directionx && directiony:
-		var direction = Vector2(directionx, directiony)
-		print(velocity.normalized().angle() - direction.normalized().angle())
+	
+	var maxRadians = deg_to_rad(maxDegreesPerSecond * delta)
+	var directionx = 0
+	var directiony = 0
+	directionx = Input.get_axis("ui_left", "ui_right")
+	directiony = Input.get_axis("ui_down", "ui_up")
+	if !(directionx == 0 && directiony == 0):
+		var direction = Vector2(directionx, -directiony)
+		#var anglediff = wrapf(velocity.angle() - direction.angle(), -PI, PI)
+		var anglediff = wrapf(direction.angle() - velocity.angle(), -PI, PI)
+		var anglechange = clamp(anglediff, -maxRadians, maxRadians)
+		velocity = velocity.rotated(anglechange)
+		print(rad_to_deg(anglediff))
+		#print(direction.normalized().angle() - velocity.normalized().angle())
 		#velocity.x += direction * SPEED * 0.05 * delta

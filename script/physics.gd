8048e02759e06bcdec9ce3eb407698fbab215cee
linkman <linkman8912@proton.me>
2025-10-30 12:58:28 -0400

    remove print statements

--- script/physics.gd
+++ script/physics.gd
@@ -37,7 +37,7 @@ func _physics_process(delta: float) -> void:
 			#characterBody.velocity.x = move_toward(characterBody.velocity.x, speed * 2, slow)
 		#if characterBody.velocity.x < speed * -1:
 			#characterBody.velocity.x = move_toward(characterBody.velocity.x, speed * -2, slow)
-		print(rigidBody.linear_velocity.length())
+		#print(rigidBody.linear_velocity.length())
 func _bounce(body: Node2D) -> void:
 	if body:
 		pass

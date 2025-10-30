850315dfc72c4917953babc884857922d07b8ad7
linkman <linkman8912@proton.me>
2025-10-30 12:56:33 -0400

    complete mainphysics

---
+++ script/characterphysics.gd
@@ -0,0 +1,25 @@
+extends RigidBody2D
+
+const maxDegreesPerSecond = 90
+
+func _ready():
+	pass
+	
+func _physics_process(delta: float) -> void:
+	
+	var maxRadians = deg_to_rad(maxDegreesPerSecond * delta)
+	var directionx = 0
+	var directiony = 0
+	directionx = Input.get_axis("ui_left", "ui_right")
+	directiony = Input.get_axis("ui_down", "ui_up")
+	if !(directionx == 0 && directiony == 0):
+		var direction = Vector2(directionx, -directiony)
+		#var anglediff = wrapf(velocity.angle() - direction.angle(), -PI, PI)
+		var anglediff = wrapf(direction.angle() - linear_velocity.angle(), -PI, PI)
+		var anglechange = clamp(anglediff, -maxRadians, maxRadians)
+		linear_velocity = linear_velocity.rotated(anglechange)
+		#print(rad_to_deg(anglediff))
+	#print(velocity.length())
+		#print(direction.normalized().angle() - velocity.normalized().angle())
+		#velocity.x += direction * SPEED * 0.05 * delta
+	

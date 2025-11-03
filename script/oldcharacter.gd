d88c4d4e9210f652bbe4870dcdb148403596b574
BrysonSGuthrie <guthriebryson@gmail.com>
2025-11-03 10:55:46 -0500

    sending gore to andrew

--- script/oldcharacter.gd
+++ script/oldcharacter.gd
@@ -1,4 +1,4 @@
-extends CharacterBody2D
+extends RigidBody2D
 
 
 const SPEED = 400.0
@@ -9,9 +9,9 @@ func _ready() -> void:
 	
 	
 func _physics_process(delta: float) -> void:
-	
 	var maxRadians = deg_to_rad(maxDegreesPerSecond * delta)
-	var directionx = 0
+	var directionx = Input.get_axis("ui_left", "ui_right")
+	linear_velocity = linear_velocity.rotated(maxRadians * directionx)
 	var directiony = 0
 	directionx = Input.get_axis("ui_left", "ui_right")
 	directiony = Input.get_axis("ui_down", "ui_up")

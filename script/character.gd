a8d117a227bc12edf7d3f960ae8073bd6dc833f3
BrysonSGuthrie <guthriebryson@gmail.com>
2025-11-04 11:10:14 -0500

    home

--- script/character.gd
+++ script/character.gd
@@ -1,14 +1,16 @@
 extends RigidBody2D
 
-const maxDegreesPerSecond = 90
+const maxDegreesPerSecond = 7
+var control = 10
 
-
+func set_control(c):
+	control = float(c)
 
 
 
 func _physics_process(delta: float) -> void:
 	
-	var maxRadians = deg_to_rad(maxDegreesPerSecond * delta)
+	var maxRadians = deg_to_rad((maxDegreesPerSecond*control) * delta)
 	var directionx = Input.get_axis("ui_left", "ui_right")
 	linear_velocity = linear_velocity.rotated(maxRadians * directionx)
 	#print(rad_to_deg(directionx))

ac12b0e10fda096156b2fe629a00fd2c6b315036
BrysonSGuthrie <guthriebryson@gmail.com>
2025-11-05 08:51:26 -0500

    how to make a sword

--- script/hitbox.gd
+++ script/hitbox.gd
@@ -13,8 +13,11 @@ func _on_area_entered(area: Area2D) -> void:
 	var collider = area.get_parent() # often the main node that owns the area
 	#print("Collided with: ", collider.name)
 	var attack_node = collider.get_node_or_null("attack_node")
-	
-	if attack_node:
+	var main = get_parent().get_parent()
+	var player = main.get_node_or_null("Player")
+	var enemy = main.get_node_or_null("Enemy")
+
+	if attack_node and (not collider == player or collider == enemy):
 		print(attack_node.get_parent())
 		var attack = attack_node.attack_points()
 		damage(attack) 

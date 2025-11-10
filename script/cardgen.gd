cfbc86bcb8ddad6067636242f57b1a5c23c3181c
Ella.Ship.it <ezemllerwu@gmail.com>
2025-11-09 20:17:18 -0500

    gay

--- script/cardgen.gd
+++ script/cardgen.gd
@@ -3,9 +3,10 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	var data = JSON.parse_string(load_from_file())
+	
+	
+func load_from_file():
+	var file = FileAccess.open("res://data/formattedCards.json", FileAccess.READ)
+	var content = file.get_as_text()
+	return content

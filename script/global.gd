2a7927571b3ff43a85fd02d6b3db470cb8eeb887
ezemllerwu-ship-it <ezemllerwu@gmail.com>
2025-11-12 09:42:48 -0500

    fuck

---
+++ script/global.gd
@@ -0,0 +1,23 @@
+extends Node
+
+
+func shop():
+	pass
+	
+func battle():
+	pass
+
+func start():
+	pass
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass

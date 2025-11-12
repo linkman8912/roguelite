f4d39f2132532644469e49ccaec5669bbe58d19e
Ella.Ship.it <ezemllerwu@gmail.com>
2025-11-11 20:53:15 -0500

    added sprights

--- script/cardgen.gd
+++ script/cardgen.gd
@@ -4,9 +4,11 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var data = JSON.parse_string(load_from_file())
-	
+	cardart(data[0][0]["art"])
 	
 func load_from_file():
 	var file = FileAccess.open("res://data/formattedCards.json", FileAccess.READ)
 	var content = file.get_as_text()
 	return content
+func cardart(art):
+	var texture = load(art)

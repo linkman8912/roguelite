379747a9b1aa0d09d61ea183fa38157b7c533f5b
linkman <linkman8912@proton.me>
2025-11-18 09:39:24 -0500

    fix == bug

--- script/cardgen.gd
+++ script/cardgen.gd
@@ -8,15 +8,16 @@ var rarities = [
 ]
 
 @onready var main = $"/root/Main"
-var rarity = 0
-var card = 0
+var rarity: int
+var card: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	rarity = main.generate_rarity()
 	card = main.generate_card()
 	while (isDupe()):
-		card = main.generate_card
+		card = main.generate_card()
+		print("isDupe")
 	set_card(rarity, card)
 
 
@@ -38,9 +39,9 @@ func isDupe():
 	var card1 = $"../../Container1/Control"
 	var card2 = $"../../Container2/Control2"
 	var card3 = $"../../Container3/Control3"
-	var dupe1 = (rarity == int(card1.rarity) && card == int(card1.card) && !(name == "Control"))
-	var dupe2 = (rarity == int(card2.rarity) && card == int(card2.card) && !(name == "Control2"))
-	var dupe3 = (rarity == int(card3.rarity) && card == int(card3.card) && !(name == "Control3"))
+	var dupe1 = (rarity == card1.rarity && card == card1.card && !(name == "Control"))
+	var dupe2 = (rarity == card2.rarity && card == card2.card && !(name == "Control2"))
+	var dupe3 = (rarity == card3.rarity && card == card3.card && !(name == "Control3"))
 	return dupe1 || dupe2 || dupe3
 
 func _on_button_pressed() -> void:
